a4fb1a7ed79d4ebf627eec730c2f4a5142e660c0
Cristobal <cridonoso@udec.cl>
2020-12-12 18:04:20 -0300

    time

--- Presentation/get_data.py
+++ Presentation/get_data.py
@@ -3,13 +3,17 @@ sys.path.append(os.path.realpath('.'))
 from Domain.entities import AlphaExtractor
 import multiprocessing
 from Domain import utils
+from time import time
 
-
-# ipc_frame = utils.get_IPC()
-# ipc_frame.to_csv('Domain/data/ipc.csv', index=False)
+t0 = time()
+#ipc_frame = utils.get_IPC()
+#ipc_frame.to_csv('Domain/data/ipc.csv', index=False)
 
 alpha = AlphaExtractor(start_date='2020-04-02')
 num_cores = multiprocessing.cpu_count()
 
 alpha.extract_features(n_jobs=num_cores)
 alpha.to_pickle('Domain/data/dataset_new.pkl')
+t1 = time()
+
+print('Feature extraction took {:.2f} sec on {} cores'.format((t1-t0), num_cores))
