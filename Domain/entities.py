54baaa83e068a600a5d5dda0eedbd7e78fbcbe14
Cristobal <cridonoso@udec.cl>
2020-12-19 17:57:05 -0300

    only covid

--- Domain/entities.py
+++ Domain/entities.py
@@ -7,6 +7,61 @@ from Domain import features, utils
 from joblib import Parallel, delayed
 
 
+class OnlyCovidExtractor(Extractor):
+    """ AlphaExtractor """
+
+    def __init__(self,
+                 **kwargs):
+        """ Alpha Extractor
+
+        Root class attributes
+        ----------
+        start_date
+        end_date
+        quarantines
+        comuna_to_region
+        population
+
+        """
+        super(AlphaExtractor, self).__init__(**kwargs)
+        self.features_list = [features.casos_totales_por_comuna,
+                              features.casos_totales_por_comuna_cumulativo,
+                              features.casos_totales_por_region_cumulativo,
+                              features.casos_totales_nacional,
+                              features.PCR_por_region,
+                              features.UCI_por_region,
+                              features.UCI_etario,
+                              features.fallecidos_etario,
+                              features.casos_nuevos_por_region_cumulativo,
+                              features.fallecidos_por_region_cumulativo,
+                              features.ventiladores_nacional,
+                              features.movilidad_comuna,
+                              features.fallecidos_por_comuna,
+                              features.disponibilidad_camas_UCi_region,
+                              features.positividad_por_comuna]
+
+    def get_features(self, date, comuna, region):
+        num_cores = multiprocessing.cpu_count()
+        N = self.population[self.population['Comuna'] == comuna]['Poblacion'].values
+
+        if N.shape[0] == 0:
+            return [], []
+
+        response = Parallel(n_jobs=num_cores)(delayed(fn)(date, comuna, region) \
+                            for fn in self.features_list)
+
+        values = np.concatenate([x[0] for x in response])
+        feat_names = np.concatenate([x[1] for x in response])
+        return values, feat_names
+
+
+    def get_label(self, date, comuna):
+        comuna_df = self.quarantines[self.quarantines['Nombre'] == comuna]
+        cond = (date >= comuna_df['Fecha de Inicio']) & (date <= comuna_df['Fecha de Término'])
+        y = int(cond.any())
+        return y
+
+
 class AlphaExtractor(Extractor):
     """ AlphaExtractor """
 
--- Presentation/get_data.py
+++ Presentation/get_data.py
@@ -1,6 +1,6 @@
 import sys,os
 sys.path.append(os.path.realpath('.'))
-from Domain.entities import AlphaExtractor
+from Domain.entities import AlphaExtractor, OnlyCovidExtractor
 import multiprocessing
 from Domain import utils
 from time import time
@@ -9,11 +9,11 @@ t0 = time()
 ipc_frame = utils.get_IPC()
 ipc_frame.to_csv('Domain/data/ipc.csv', index=False)
 
-alpha = AlphaExtractor(start_date='2020-03-10', end_date='2020-12-18')
+alpha = OnlyCovidExtractor(start_date='2020-03-10', end_date='2020-12-18')
 num_cores = multiprocessing.cpu_count()
 
 alpha.extract_features(n_jobs=num_cores)
-alpha.to_csv('Domain/data/20201219_features.csv')
+alpha.to_csv('Domain/data/20201219_features_onlycovid.csv')
 # alpha.to_pickle('Domain/data/dataset_new.pkl')
 t1 = time()
 
